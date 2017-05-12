51d2bb6fececdf3b0f1e4416270c6428c0bf6ede
Foyerry <1058494062@qq.com>
2017-05-12 14:07:07 +0800

    update 2

--- Foyerry/Foyerry.Tools/String/StringExtensions.cs
+++ Foyerry/Foyerry.Tools/String/StringExtensions.cs
@@ -10,9 +10,19 @@ namespace Foyerry.Tools.String
     {
         public static string GetItem(this string source, int itemIndex, string separator = ",")
         {
-            if (source == null) return string.Empty;
+            if (string.IsNullOrEmpty(source)) return string.Empty;
             var items = source.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries);
-            return itemIndex > items.Length ? string.Empty : items[itemIndex];
+            return itemIndex > items.Length
+                ? string.Empty
+                : items[itemIndex];
+        }
+
+        public static string Ellipsis(this string source, int length, int ellipsisLength)
+        {
+            if (string.IsNullOrEmpty(source)) return string.Empty;
+            return source.Length <= length
+                ? source
+                : source.Substring(0, length - ellipsisLength).PadRight(length, '.');
         }
     }
 }
--- Foyerry/Foyerry.Tools/String/StringUtility.cs
+++ Foyerry/Foyerry.Tools/String/StringUtility.cs
@@ -8,6 +8,13 @@ namespace Foyerry.Tools.String
 {
     public class StringUtility
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val"></param>
+        /// <param name="defaultVal"></param>
+        /// <returns></returns>
         public static T GetVal<T>(object val, T defaultVal)
         {
             var result = defaultVal;
@@ -21,19 +28,6 @@ namespace Foyerry.Tools.String
             }
             return result;
         }
-
-        public static T As<T>(object val, T defaultVal)
-        {
-            var result = defaultVal;
-            if (val == null || val == DBNull.Value) return result;
-            try
-            {
-                result = (T)Convert.ChangeType(val, typeof(T));
-            }
-            catch (InvalidCastException)
-            {
-            }
-            return result;
-        }
+       
     }
 }
