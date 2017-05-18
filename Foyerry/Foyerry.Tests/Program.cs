9e5a270d83f239ab79565fa435278dc956fdb78d
Foyerry <1058494062@qq.com>
2017-05-18 18:34:05 +0800

    add signalR

--- Foyerry/Foyerry.Tests/Program.cs
+++ Foyerry/Foyerry.Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,44 @@ namespace Foyerry.Tests
     {
         static void Main(string[] args)
         {
-            StringUtility.As("", 0.0);
+            Console.WriteLine("Begin ...");
+
+
+            var p = Path.GetDirectoryName(@"E:\server.txt");
+            Console.WriteLine(p);
+            Console.ReadKey();
+
         }
+    }
+
+    public class Animal
+    {
+        public string name;
+        public int id;
+
+        public Animal(string n, int i)
+        {
+            name = n;
+            this.id = i;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("父类eat");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string n, int i, string ha)
+            : base(n, i)
+        {
+        }
+
+        public void Eat(int num)
+        {
+            Console.WriteLine("Cat eat");
+        }
+
     }
 }
--- Foyerry/Foyerry.Tools/String/StringExtensions.cs
+++ Foyerry/Foyerry.Tools/String/StringExtensions.cs
@@ -8,6 +8,19 @@ namespace Foyerry.Tools.String
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// 分割字符串
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string[] Items(this string source, string separator = ",")
+        {
+            return string.IsNullOrEmpty(source)
+                ? new string[0]
+                : source.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
         public static string GetItem(this string source, int itemIndex, string separator = ",")
         {
             if (string.IsNullOrEmpty(source)) return string.Empty;
@@ -24,5 +37,6 @@ namespace Foyerry.Tools.String
                 ? source
                 : source.Substring(0, length - ellipsisLength).PadRight(length, '.');
         }
+
     }
 }
