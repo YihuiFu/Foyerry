581bb7ebbe321af820ee872caa5aa448942116cc
fuyh <1058494062@qq.com>
2018-02-05 14:16:52 +0800

    SingletonProvider

---
+++ Foyerry/Foyerry.DesignPattern/SingletonProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foyerry.DesignPattern
+{
+    public class SingletonProvider<T> where T : new()
+    {
+        private SingletonProvider()
+        {
+        }
+
+        public static T Instance
+        {
+            get { return SingletonCreator.instance; }
+        }
+
+        private class SingletonCreator
+        {
+            static SingletonCreator() { }
+
+            internal static readonly T instance = new T();
+        }
+    }
+}
